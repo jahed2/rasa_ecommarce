d066f77ab9a954c09fc759f13cf61ff5a04dd55b
noman siddique <48939978+jahed2@users.noreply.github.com>
2021-08-23 11:46:49 +0600

    Add files via upload

--- actions/actions.py
+++ actions/actions.py
@@ -32,6 +32,8 @@
 #         return []
 
 from typing import Any, Text, Dict, List
+import rasa.core.tracker_store  
+from rasa.shared.core.trackers import DialogueStateTracker 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet, EventType
 from rasa_sdk.executor import CollectingDispatcher
@@ -41,6 +43,7 @@ from email.mime.text import MIMEText
 import asyncio
 from twilio.rest import Client
 import os
+import json
 
 class ValidateRestaurantForm(Action):
     def name(self)-> Text:
@@ -71,7 +74,7 @@ class ActionSubmit(Action):
         #     tracker.get_slot("email"),
         #     tracker.get_slot("subject"),
         #     tracker.get_slot("message")
-        
+        # )
         dispatcher.utter_message(template="utter_details_thanks",
                                  Name=tracker.get_slot("name"),
                                  Mobile_number=tracker.get_slot("number"),
@@ -108,6 +111,72 @@ class ActionSubmit(Action):
 #     finally:
 #         #terminating the session
 #         s.quit()
+
+
+
+
+# def SendEmail(toaddr,subject,message):
+#     fromaddr = "sender_email"
+#     # instance of MIMEMultipart
+#     msg = MIMEMultipart()
+
+#     # storing the senders email address
+#     msg['From'] = fromaddr
+
+#     # storing the receivers email address
+#     msg['To'] = toaddr
+
+#     # storing the subject
+#     msg['Subject'] = subject
+
+#     # string to store the body of the mail
+#     body = message
+
+#     # attach the body with the msg instance
+#     msg.attach(MIMEText(body, 'plain'))
+
+    # open the file to be sent
+    # filename = "/home/ashish/Downloads/webinar_rasa2_0.png"
+    # attachment = open(filename, "rb")
+    #
+    # # instance of MIMEBase and named as p
+    # p = MIMEBase('application', 'octet-stream')
+    #
+    # # To change the payload into encoded form
+    # p.set_payload((attachment).read())
+    #
+    # # encode into base64
+    # encoders.encode_base64(p)
+    #
+    # p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+    #
+    # # attach the instance 'p' to instance 'msg'
+    # msg.attach(p)
+
+    # creates SMTP session
+    # s = smtplib.SMTP('nomansiddique719@gmail.com', 587)
+
+    # # start TLS for security
+    # s.starttls()
+
+
+    # # Authentication
+    # try:
+    #     s.login(fromaddr, "123456")
+
+    #     # Converts the Multipart msg into a string
+    #     text = msg.as_string()
+
+    #     # sending the mail
+    #     s.sendmail(fromaddr, toaddr, text)
+    # except:
+    #     print("An Error occured while sending email.")
+    # finally:
+    #     # terminating the session
+    #     s.quit()
+
+
+
 
 class ActionSubmit1(Action):
     def name(self)->Text:
@@ -1035,7 +1104,7 @@ class ActionCarouse13(Action):
     def name(self) -> Text:
         return "action_carousels13"
     
-    def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+    async def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
         message = {
             "type": "template",
             "payload": {
@@ -1104,7 +1173,7 @@ class ActionCarouse14(Action):
     def name(self) -> Text:
         return "action_carousels14"
     
-    def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+    async def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
         message = {
             "type": "template",
             "payload": {
@@ -1172,7 +1241,7 @@ class ActionCarouse15(Action):
     def name(self) -> Text:
         return "action_carousels15"
     
-    def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
+    async def run(self, dispatcher, tracker: Tracker, domain: "DomainDict") -> List[Dict[Text, Any]]:
         message = {
             "type": "template",
             "payload": {
@@ -1236,3 +1305,47 @@ class ActionCarouse15(Action):
 
 
 
+class ActionSaveConversation(Action):
+
+    def name(self) -> Text:
+        return "action_save_conversation"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        
+        conversation=tracker.events
+        print(conversation)
+        import os
+        if not os.path.isfile('chats.csv'):
+            with open('chats.csv','w') as file:
+                file.write("intent,user_input,entity_name,entity_value,action,bot_reply\n")
+        chat_data=''
+        for i in conversation:
+            if i['event'] == 'user':
+                chat_data+=i['parse_data']['intent']['name']+','+i['text']+','
+                print('user: {}'.format(i['text']))
+                if len(i['parse_data']['entities']) > 0:
+                    chat_data+=i['parse_data']['entities'][0]['entity']+','+i['parse_data']['entities'][0]['value']+','
+                    print('extra data:', i['parse_data']['entities'][0]['entity'], '=',
+                          i['parse_data']['entities'][0]['value'])
+                else:
+                    chat_data+=",,"
+            elif i['event'] == 'bot':
+                print('Bot: {}'.format(i['text']))
+                try:
+                    chat_data+=i['metadata']['utter_action']+','+i['text']+'\n'
+                except KeyError:
+                    pass
+        else:
+            with open('chats.csv','a') as file:
+                file.write(chat_data)
+        
+        dispatcher.utter_message(text="All Chats saved.")
+
+        return []
+
+
+
+
+    
